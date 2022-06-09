2f677f0abdd449561004a4281d25feddd9db17ba
Allison Stewart <amstewart@yahoo.com>
2022-06-08 21:15:19 -0400

    light styling for readability

--- src/components/Player.js
+++ src/components/Player.js
@@ -6,8 +6,25 @@ function Player(props) {
   
   return (
     <div>
-      Individual player stats go here
-      
+      <div className='player-wrapper'>
+      <h2>Player Stats</h2>
+        <h1>{player.fullName}</h1>
+        <img src={player.image} alt={player.fullName}/>
+      {player.map(stat => (
+        <div className='stat-wrapper' key={stat.week}>
+          <span className='opponent-wrapper'>
+        <img src={stat.teamImage} alt={stat.team}/>
+        <p>vs.</p>
+        <img src={stat.opponentImage} alt={stat.opponent}/>
+        </span>
+          <p>Week {stat.week}</p>
+          <p>{stat.gameDate}</p>
+          <p>{stat.opponent}</p>
+          <p>PsYds/Att: {Math.round(stat.PsYds / stat.Att)}</p>
+          <p>Comp%: {Math.round((stat.Cmp / stat.Att) * 100)}%</p>
+        </div>
+      ))}
+      </div>
     </div>
   )
 }
