c1e72df67c8c5830547d893294ffbbc6255b077c
Allison Stewart <amstewart@yahoo.com>
2022-06-08 21:12:44 -0400

    enabled button to fetch data for player stats

--- src/components/Player.js
+++ src/components/Player.js
@@ -3,9 +3,11 @@ import { PlayersContext, PlayerContext } from '../Context'
 
 function Player(props) {
   const { player } = useContext(PlayerContext);
+  
   return (
     <div>
       Individual player stats go here
+      
     </div>
   )
 }
--- src/components/Players.js
+++ src/components/Players.js
@@ -1,10 +1,28 @@
 import React, { useContext, useState, useEffect } from "react";
 import axios from "axios";
-import { PlayersContext } from "../Context";
+import { PlayerContext, PlayersContext } from "../Context";
 import Player from "./Player";
 
 function Players(props) {
   const { players, setPlayers } = useContext(PlayersContext);
+  const { setPlayer } = useContext(PlayerContext);
+  const [toggleOn, setToggleOn] = useState(false);
+
+  const fetchPlayer= (id) => {
+    axios.get(`https://project.trumedianetworks.com/api/nfl/player/${id}`, {
+      headers:
+      {
+        "tempToken": "1d982543-8084-47ef-bb55-2c3e84cf1668",
+        "expires": "Thu, 09 Jun 2022 21:17:35 GMT"
+      }
+    })
+    .then(res => {
+      setPlayer([...res.data])
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
 
 useEffect(() => {
   axios.get('https://project.trumedianetworks.com/api/nfl/players', {
@@ -29,10 +47,14 @@ useEffect(() => {
         <div className='player-card' key={player.playerId}>
           <img className='player-photo item' src={player.playerImage} alt={player.fullName}/>
           <h2 className='item'>{player.fullName}</h2>
-          <button>Show Stats</button>
+          <button onClick={() => {
+            fetchPlayer(player.playerId)
+            setToggleOn(true)
+            }}>
+              Show Stats</button>
         </div>
       ))}
-      <Player />
+      {toggleOn && <Player />}
     </div>
   )
 }
